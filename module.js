e4456218a2027b0cfea408b9d0223fd8f7e6df44
Mohd Salim <smsalim931@gmail.com>
2025-05-10 23:07:35 +0530

    first commit

--- module.js
+++ module.js
@@ -1,9 +1,16 @@
 const http = require('http')
+const fs = require('fs')
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST || 'localhost'
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello World</h1>')
+  fs.readFileSync('test.html')
+  console.log(fs.toString())
 })
 
-server.listen(3000)
+server.listen(3000, () => {
+  console.log(`Server running at http://${host}:${port}/`)
+})
